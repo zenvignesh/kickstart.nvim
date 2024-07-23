d00e6741696d597303a3dbb93493e9c39a533c67
Vignesh Subbaiyan <zenvignesh@gmail.com>
2024-07-23 23:32:48 +0200

    Markdown plugin and latex plugin implementation

---
+++ lua/custom/plugins/latex_plugin.lua
@@ -0,0 +1,26 @@
+local function compile_latex()
+  vim.cmd 'write'
+
+  local filename = vim.fn.expand '%'
+  local filepath = vim.fn.expand '%:p:h'
+
+  os.execute('pdflatex -interaction=nonstopmode -output-directory=' .. vim.fn.shellescape(filepath) .. ' ' .. filename .. ' > /dev/null 2>&1')
+
+  -- This compilation need to be done twise, to get the page numbers work correctly
+  os.execute('pdflatex -interaction=nonstopmode -output-directory=' .. vim.fn.shellescape(filepath) .. ' ' .. filename .. ' > /dev/null 2>&1')
+
+  local temp_files = { '.aux', '.log', '.nav', '.out', '.snm', '.toc' }
+
+  for _, ext in ipairs(temp_files) do
+    -- cool = vim.fn.shellescape(filepath .. '/' .. vim.fn.expand '%:t:r' .. ext)
+    -- print(cool)
+    os.execute('rm -f ' .. vim.fn.shellescape(filepath .. '/' .. vim.fn.expand '%:t:r' .. ext))
+  end
+
+  print 'LaTeX file compiled!'
+end
+
+vim.api.nvim_create_autocmd('BufWritePost', {
+  pattern = '*.tex',
+  callback = compile_latex,
+})
