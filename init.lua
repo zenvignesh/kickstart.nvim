34d70ea0625d0ca37e428d45a5e9c5183f96bac4
Vignesh Subbaiyan <59747950+zenvignesh@users.noreply.github.com>
2024-12-15 10:19:13 +0100

    Add Vim Dadbod - SQL Plugin

--- init.lua
+++ init.lua
@@ -928,6 +928,7 @@ require('lazy').setup({
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'kickstart.plugins.w3m-vim',
+  require 'kickstart.plugins.dadbod',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
