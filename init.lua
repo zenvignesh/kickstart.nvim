8e76ea0975d9139ce4bba5b4d4de6d5c5a1b443b
Vignesh Subbaiyan <59747950+zenvignesh@users.noreply.github.com>
2025-01-16 20:38:04 +0100

    C program format Enable

--- init.lua
+++ init.lua
@@ -711,11 +711,17 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        c = { 'clang_format' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
+      },
+      formatters = {
+        clang_format = {
+          prepend_args = { '--style=file:/home/vignesh/.config/nvim/lua/custom/config/.clang-format' },
+        },
       },
     },
   },
