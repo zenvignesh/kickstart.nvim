98b040064f42e7add2c6fe9751e6d4207222720f
Vignesh Subbaiyan <59747950+zenvignesh@users.noreply.github.com>
2025-05-09 20:34:23 +0200

    Fix the workspace after sync fork issues

--- init.lua
+++ init.lua
@@ -720,6 +720,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'prettier', -- Used to format markdown file
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
@@ -760,7 +761,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = { c = false, cpp = false }
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
         else
